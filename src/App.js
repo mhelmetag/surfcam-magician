86452c02575b28b232424835763b8f75b53b81c1
Max Helmetag <mhelmetag@gmail.com>
2018-10-03 14:16:19 -0700

    garbage stream url input and refresh

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import ReactPlayer from 'react-player';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleStreamUrlUpdate = this.handleStreamUrlUpdate.bind(this);
+  }
+
+  componentWillMount() {
+    this.setState({streamUrl: "https://cams.cdn-surfline.com/abr-3/ngrp:sandspitcam_all/playlist.m3u8"})
+  }
+
+  handleStreamUrlUpdate(e) {
+    e.preventDefault();
+
+    if (e.key === 'Enter') {
+      this.setState({streamUrl: e.currentTarget.value});
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>Surfcam Magician</p>
-        </header>
+        <p>Surfcam Magician</p>
+        <div className="SurfCam">
+          <ReactPlayer
+            url={this.state.streamUrl}
+            playing={true}
+            controls={true}
+          />
+        </div>
+        <div className="StreamUrlInput">
+          <input
+            type="text"
+            placeholder={this.state.streamUrl}
+            onKeyPress={this.handleStreamUrlUpdate}
+          />
+        </div>
       </div>
     );
   }
