21e79038ddf4a10bc653af2d5d478c59c421a809
Sean Spearman <smspearmandev@gmail.com>
2025-08-31 13:19:44 -0700

    Spoof UA for hls requests

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,8 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-function onProxyRes(proxyRes, req, res) {
+const UA_HEADER_NAME = "user-agent";
+
+function logRes(proxyRes, req, res) {
   console.log(`\n<<< Raw Response from Target >>>`);
   console.log(`Status: ${proxyRes.statusCode}`);
   console.log(`Headers:`, proxyRes.headers);
@@ -13,7 +15,7 @@ function onProxyRes(proxyRes, req, res) {
   });
 }
 
-function onProxyReq(proxyReq, req, res) {
+function logReq(proxyReq, req, res) {
   console.log(`\n--- Proxying Request ---`);
   console.log(`From: ${req.headers.host}`);
   console.log(`To: ${proxyReq.protocol}//${proxyReq.host}${proxyReq.path}`);
@@ -21,23 +23,23 @@ function onProxyReq(proxyReq, req, res) {
 
 module.exports = function (app) {
   // Logs *every* incoming request before proxying/static handling
-  app.use((req, res, next) => {
-    console.log(`\n=== Incoming Request ===`);
-    console.log(`Method: ${req.method}`);
-    console.log(`URL: ${req.url}`);
-    console.log(`Origin: ${req.headers.origin}`);
-    console.log(`Host: ${req.headers.host}`);
-    console.log(`Headers:`, req.headers);
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   console.log(`\n=== Incoming Request ===`);
+  //   console.log(`Method: ${req.method}`);
+  //   console.log(`URL: ${req.url}`);
+  //   console.log(`Origin: ${req.headers.origin}`);
+  //   console.log(`Host: ${req.headers.host}`);
+  //   console.log(`Headers:`, req.headers);
+  //   next();
+  // });
 
   app.use(
     "/kbyg",
     createProxyMiddleware({
       target: "https://services.surfline.com",
       changeOrigin: true,
-      onProxyReq,
-      onProxyRes,
+      // onProxyReq: logReq,
+      // onProxyRes: logRes,
     })
   );
 
@@ -46,6 +48,8 @@ module.exports = function (app) {
     createProxyMiddleware({
       target: "https://services.surfline.com",
       changeOrigin: true,
+      // onProxyReq: logReq,
+      // onProxyRes: logRes,
     })
   );
 
@@ -54,6 +58,8 @@ module.exports = function (app) {
     createProxyMiddleware({
       target: "https://cams.cdn-surfline.com",
       changeOrigin: true,
+      // onProxyReq: logReq,
+      // onProxyRes: logRes,
     })
   );
 
@@ -62,6 +68,21 @@ module.exports = function (app) {
     createProxyMiddleware({
       target: "https://hls.cdn-surfline.com",
       changeOrigin: true,
+      onProxyReq: (proxyReq, req, res) => {
+        logReq(proxyReq, req, res);
+
+        const ua = req.headers[UA_HEADER_NAME];
+        // console.log("Headers", req.headers);
+        // const proxyUa = proxyReq.headers["User-Agent"];
+
+        // console.log(UA_HEADER_NAME, ua);
+        // console.log("proxy User-Agent", proxyUa);
+        proxyReq.setHeader(
+          UA_HEADER_NAME,
+          "Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-G973U) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/14.2 Chrome/87.0.4280.141 Mobile Safari/537.36"
+        );
+      },
+      onProxyRes: logRes,
     })
   );
 };
