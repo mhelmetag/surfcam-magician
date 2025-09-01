cdfaef116ef78ee06adbc899b13d507db38eda72
Sean Spearman <smspearmandev@gmail.com>
2025-09-01 12:33:33 -0700

    Turn proxy logs off

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ const UA_HEADER_NAME = "user-agent";
 const UA_SPOOF =
   "Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-G973U) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/14.2 Chrome/87.0.4280.141 Mobile Safari/537.36";
 
-const DEBUG = true;
+const DEBUG = false;
 const log = (...args) => {
   if (DEBUG) {
     console.log(...args);
